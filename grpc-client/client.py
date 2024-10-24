671c85cb43059cfbcb2cc0abf1947d6d89d1b5de
MNHR <matias.trabajo16@gmail.com>
2024-10-24 00:35:31 -0300

    grpc cliente-servidor

---
+++ grpc-client/client.py
@@ -0,0 +1,15 @@
+import grpc
+from grpc-server.proto import servicio_pb2
+from grpc-server.proto import servicio_pb2_grpc
+
+def run():
+    # Crear el canal de conexión al servidor gRPC
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = servicio_pb2_grpc.PedidoServiceStub(channel)
+        # Enviar una solicitud al servidor gRPC
+        response = stub.CrearPedido(servicio_pb2.PedidoRequest(nombreProducto="Laptop", precio=999.99))
+        print("Estado del pedido: " + response.estado)
+
+if __name__ == '__main__':
+    run()
+
